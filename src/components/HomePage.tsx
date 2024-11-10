f5a1b31984681e69ddd3b589cffd21f40ef4234b
Darko Luketic <info@icod.de>
2024-11-10 19:27:09 +0100

    good now

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,16 +1,6 @@
 import React, { useMemo } from "react";
 import { iso } from "@iso";
 
-function nonNullable<T>(value: T): value is NonNullable<T> {
-  return value != null;
-}
-
-function toSorted<T>(arr: T[], comparator: (a: T, b: T) => number): T[] {
-  const sorted = [...arr];
-  sorted.sort(comparator);
-  return sorted;
-}
-
 export const HomePage = iso(`
   field Query.HomePage @component {
     blogs{
